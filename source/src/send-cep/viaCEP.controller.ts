8a4f8a07e4b91cec66aa8d5f266313cf69196feb
Augusto Rodrigues <augustofeliperodrigues1999@gmail.com>
2023-01-30 14:46:18 -0300

    feat(endpoint): Método GET para ceps através da UF

--- source/src/send-cep/viaCEP.controller.ts
+++ source/src/send-cep/viaCEP.controller.ts
@@ -3,10 +3,19 @@ import { ViaCEPService } from './viaCEP.service';
 
 @Controller('viaCEP')
 export class ViaCEPController {
-  constructor(private readonly sendCepService: ViaCEPService) {}
+  constructor(private readonly viaCepService: ViaCEPService) {}
 
   @Get('/getInfo/:cep')
   async getCep(@Param('cep') cep: string) {
-    return await this.sendCepService.getCep(cep);
+    return await this.viaCepService.getCep(cep);
+  }
+
+  @Get('/getInfo/:uf/:city/:logradouro')
+  async getCepFromUf(
+    @Param('uf') uf: string,
+    @Param('city') city: string,
+    @Param('logradouro') logradouro: string,
+  ) {
+    return await this.viaCepService.getCepFromUf(uf, city, logradouro);
   }
 }
--- source/src/send-cep/viaCEP.service.ts
+++ source/src/send-cep/viaCEP.service.ts
@@ -7,18 +7,36 @@ import { catchError, lastValueFrom } from 'rxjs';
 export class ViaCEPService {
   constructor(private httpService: HttpService) {}
 
-  private readonly logger = new Logger(ViaCEPService.name)
-  http: string = process.env.CPE_BASE_URL;
+  private readonly logger = new Logger(ViaCEPService.name);
+  http: string = process.env.CEP_BASE_URL;
 
-  async getCep(cpe: string): Promise<Cep.CepResponse> {
+  async getCep(cep: string): Promise<Cep.CepResponse> {
     const { data } = await lastValueFrom<{ data: Cep.CepResponse }>(
-      this.httpService.get(`${this.http}/${cpe}/json`).pipe(
+      this.httpService.get(`${this.http}/${cep}/json`).pipe(
         catchError((error: AxiosError) => {
           throw new HttpException(error.response.data, error.response.status);
         }),
       ),
     );
-    console.log(data);
+    this.logger.log('Info', data);
+    return data;
+  }
+
+  async getCepFromUf(
+    uf: string,
+    city: string,
+    logradouro: string,
+  ): Promise<Cep.CepResponse[]> {
+    const { data } = await lastValueFrom<{ data: Cep.CepResponse[] }>(
+      this.httpService
+        .get(`${this.http}/${uf}/${city}/${logradouro}/json`)
+        .pipe(
+          catchError((error: AxiosError) => {
+            throw new HttpException(error.response.data, error.response.status);
+          }),
+        ),
+    );
+    this.logger.log('Info', data);
     return data;
   }
 }
